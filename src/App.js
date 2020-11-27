c425f50027db70fdb38e614900d6ec003c3cabdf
brianwilgus <brianawilgus@gmail.com>
2020-11-27 15:57:05 -0500

    added react-bootstrap, sass, toast example to index

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Toast from 'react-bootstrap/Toast';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+
+const ExampleToast = ({ children }) => {
+  const [show, toggleShow] = useState(true);
+
+  return (
+    <>
+      {!show && <Button onClick={() => toggleShow(true)}>Show Toast</Button>}
+      <Toast show={show} onClose={() => toggleShow(false)}>
+        <Toast.Header>
+          <strong className="mr-auto">React-Bootstrap</strong>
+        </Toast.Header>
+        <Toast.Body>{children}</Toast.Body>
+      </Toast>
+    </>
+  );
+};
 
 function App() {
   return (
     <div className="App">
+      <Container className="p-3">
+        <Jumbotron>
+          <h1 className="header">Welcome To React-Bootstrap</h1>
+          <ExampleToast>
+            We now have Toasts
+            <span role="img" aria-label="tada">
+              🎉
+            </span>
+          </ExampleToast>
+        </Jumbotron>
+      </Container>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
